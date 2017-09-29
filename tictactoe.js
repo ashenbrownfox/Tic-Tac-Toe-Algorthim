7f24e3cfe21ad68d09e6d2dfc473e07985e69c50
ashenbrownfox <newhoopsdynasty@gmail.com>
2017-09-29 17:49:20 -0400

    Finished human side, determine winner

--- tictactoe.js
+++ tictactoe.js
@@ -31,6 +31,31 @@ function turnClick(square) {
 
 function turn(squareId, player) {
 	origBoard[squareId] = player;
-	document.getElementById(squareId).innerText = player;
+    document.getElementById(squareId).innerText = player;
+    let gameWon = checkWinCondition(origBoard, player)
+    if ( gameWon) gameOver(gameWon)
 }
 
+function checkWinCondition(board, player){
+    let plays = board.reduce((a,e,i)=>
+     (e === player) ? a.concat(i) : a,[]);
+    //array element, index
+    let gameWon = null;
+    for(let[index, win] of winCombos.entries()){
+        if(win.every(elem=>plays.indexOf(elem)>-1)) {
+            gameWon = {index: index, player: player};
+            break;
+        }
+    }
+    return gameWon;
+}
+
+function gameOver(gameWon){
+    for(let index of winCombos[gameWon.index]){
+        document.getElementById(index).style.backgroundColor =
+        gameWon.player == huPlayer ? "blue" : "red";
+    }
+    for(var i =0; i < cells.length; i++){
+        cells[i].removeEventListener('click', turnClick, false);
+    }
+}
